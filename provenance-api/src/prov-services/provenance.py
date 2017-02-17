4b0c0c297ba0d46dc86f0c6d36ce0cbe5d4b122b
aspinuso <spinuso@knmi.nl>
2017-02-17 19:45:56 +0100

    included location on derived data resultset

--- provenance-api/src/prov-services/provenance.py
+++ provenance-api/src/prov-services/provenance.py
@@ -972,7 +972,7 @@ class ProvenanceStore(object):
     
     def getDerivedDataTrace(self, id,level):
          
-        xx = self.lineage.find_one({"streams.id":id},{"runId":1,"derivationIds":1,'streams.port':1});
+        xx = self.lineage.find_one({"streams.id":id},{"runId":1,"derivationIds":1,'streams.port':1,'streams.location':1});
         xx.update({"dataId":id})
         cursor=self.lineage.find({"derivationIds":{'$elemMatch':{"DerivedFromDatasetID":id}}},{"runId":1,"streams":1});
          
