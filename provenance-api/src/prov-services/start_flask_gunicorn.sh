33cb65bc43753fd3968f6e60344907e35d7a070b
aspinuso <spinuso@knmi.nl>
2017-05-08 19:06:25 +0200

    gunicorn bootstrap for mongoclients

--- provenance-api/src/prov-services/start_flask_gunicorn.sh
+++ provenance-api/src/prov-services/start_flask_gunicorn.sh
@@ -2,4 +2,4 @@
 #gunicorn -w 4 -b 127.0.0.1:8082 flask_raas:app 
 export RAAS_LOGGING="True"
 export RAAS_REPO="mongodb://127.0.0.1/verce-prov"
-gunicorn -w 10 -b 127.0.0.1:8082 flask_raas:app --log-level debug --timeout 120 --error-logfile error.log --log-file access.log &
+gunicorn -w 9 -b 127.0.0.1:8082 "flask_raas:bootstrap_app()" --log-level debug --backlog 0 --timeout 120 --error-logfile error.log --log-file access.log &
