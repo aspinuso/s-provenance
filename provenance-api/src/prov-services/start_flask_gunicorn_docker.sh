b06976e2fa80730fd888f02c04d0f9c595238db6
andrejsim <andrej@knmi.nl>
2017-10-10 10:31:46 +0200

    mongo split to parallel docker, communication via bridge0 network.

--- provenance-api/src/prov-services/start_flask_gunicorn_docker.sh
+++ provenance-api/src/prov-services/start_flask_gunicorn_docker.sh
@@ -1,5 +1,5 @@
 #python flask_raas.py mongodb://127.0.0.1/verce-prov True
 #gunicorn -w 4 -b 127.0.0.1:8082 flask_raas:app 
 export RAAS_LOGGING="True"
-export RAAS_REPO="mongodb://127.0.0.1/$SPROV_DB"
+export RAAS_REPO="mongodb://172.17.0.1/$SPROV_DB" # docker0 bridge network used for dual container case
 gunicorn -w 9 -b 0.0.0.0:8082 "flask_raas:bootstrap_app()" --log-level debug --backlog 0 --timeout 120 --error-logfile error.log --log-file access.log
